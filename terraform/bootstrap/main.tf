af2c46c64754c4a7b9a3fb5dda9e6e3d22604df8
Quoc.VanD <quoc.vand@homecredit.vn>
2026-08-18 15:22:09 +0700

    fix(terraform): format terraform and sync oidc audience

--- terraform/bootstrap/main.tf
+++ terraform/bootstrap/main.tf
@@ -166,10 +166,7 @@ data "tls_certificate" "github_actions" {
 
 resource "aws_iam_openid_connect_provider" "github_actions" {
   url            = "https://token.actions.githubusercontent.com"
-  client_id_list = [
-    "sts.amazonaws.com",
-    "https://github.com/${var.github_org}"
-  ]
+  client_id_list = ["sts.amazonaws.com"]
   thumbprint_list = distinct(concat(
     [for cert in data.tls_certificate.github_actions.certificates : cert.sha1_fingerprint],
     ["6938fd4d98bab03faadb97b34396831e3780aea1", "1c5876a972c729e8c39e31d4f0d3674681647414"]
@@ -195,10 +192,7 @@ resource "aws_iam_role" "github_actions" {
         Action = "sts:AssumeRoleWithWebIdentity"
         Condition = {
           StringEquals = {
-            "token.actions.githubusercontent.com:aud" = [
-              "sts.amazonaws.com",
-              "https://github.com/${var.github_org}"
-            ]
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
           }
           StringLike = {
             "token.actions.githubusercontent.com:sub" = "repo:quocvand1612/*"
